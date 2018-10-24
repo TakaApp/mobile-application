24f936e31e039299b2c1243c8205249f93c483b1
Aksels Ledins <aksels.ledins@gmail.com>
2018-10-24 21:53:55 +0200

    (s) ItineraryScreen : better header

--- screens/ItineraryScreen/index.js
+++ screens/ItineraryScreen/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ScrollView, StyleSheet, View, Text } from 'react-native';
+import moment from 'moment';
 
 import LegType from '../../components/LegType';
 
@@ -12,32 +13,57 @@ export default class ItineraryScreen extends React.Component {
     const { navigation } = this.props;
     const i = navigation.getParam('itinerary', {});
 
+    console.log('i', i);
+
     return (
       <ScrollView style={styles.container}>
         <View>
-          <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              backgroundColor: '#000',
+              paddingTop: 16,
+              paddingBottom: 16,
+            }}>
             <View
               style={{
-                flex: 4,
-                display: 'flex',
-                flexDirection: 'row',
+                alignContent: 'center',
+                paddingLeft: 8,
+                paddingRight: 8,
               }}>
-              {/* Legs Walk > Bus > Walk ... */}
-              {i.legs.map((leg, index) => (
-                <View
-                  key={index}
-                  style={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    alignContent: 'center',
-                    alignItems: 'center',
-                  }}>
-                  <LegType leg={leg} />
-                  {index !== i.legs.length - 1 && <Text style={{ color: '#CCCCCC' }}>{' > '}</Text>}
-                </View>
-              ))}
+              <Text style={{ color: '#FFF' }}>Depart à</Text>
+              <Text style={styles.departureTime}>{moment(i.startTime).format('HH:mm')}</Text>
             </View>
-            <Text style={{ flex: 1, padding: 4 }}>{Math.round(i.duration / 60)} min ></Text>
+            <View>
+              <Text style={{ flex: 1, padding: 4, color: '#FFF' }}>
+                Durée: {Math.round(i.duration / 60)} min ·{' '}
+                <Text style={{ color: '#1e88e5' }}>{moment(i.endTime).format('HH:mm')}</Text>
+              </Text>
+              <View
+                style={{
+                  flex: 4,
+                  display: 'flex',
+                  flexDirection: 'row',
+                }}>
+                {/* Legs Walk > Bus > Walk ... */}
+                {i.legs.map((leg, index) => (
+                  <View
+                    key={index}
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'row',
+                      alignContent: 'center',
+                      alignItems: 'center',
+                    }}>
+                    <LegType leg={leg} />
+                    {index !== i.legs.length - 1 && (
+                      <Text style={{ color: '#CCCCCC' }}>{' > '}</Text>
+                    )}
+                  </View>
+                ))}
+              </View>
+            </View>
           </View>
           {/* Leg details */}
           <View>
@@ -49,6 +75,7 @@ export default class ItineraryScreen extends React.Component {
               </View>
             ))}
           </View>
+          !{' '}
         </View>
       </ScrollView>
     );
@@ -60,5 +87,9 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 15,
     backgroundColor: '#fff',
+  },
+  departureTime: {
+    fontSize: 32,
+    color: '#FFF',
   },
 });

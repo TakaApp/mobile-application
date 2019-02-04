1cc684394cf9a15970a0f732acd53c920ca7f20f
Aksels Ledins <aksels.ledins@gmail.com>
2019-02-04 18:41:34 +0100

    d search/selectors +getHistory

--- src/domains/search/selectors.js
+++ src/domains/search/selectors.js
@@ -4,3 +4,4 @@ export const getSearchParameters = state => get(state, 'search.parameters');
 export const getResults = state => get(state, 'search.results', []);
 export const getFormValues = state => get(state, 'search.form', {});
 export const getIsLoading = state => get(state, 'search.loading', false);
+export const getHistory = state => get(state, 'search.history', []);
